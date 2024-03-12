5503309657c6e9382dd283ed777356b690fdfe70
LeleoCriaDms <162719137+LeleoCriaDms@users.noreply.github.com>
2024-03-12 11:24:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+
+{
+    document.getElementById("imagem").src ="https://pbs.twimg.com/media/EauyQqxWkAE38Ke.png"
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://images.uncyc.org/pt/thumb/6/6c/Flakespower.png/640px-Flakespower.png"
+}
